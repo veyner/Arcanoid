e71fb926891f3053be0828a5cc07ef203f98571d
veyner <myfun239@mail.ru>
2019-04-26 19:08:49 +0300

    добавил шарик и поле для отскока, движение шарика и поля

--- Arcanoid/Form1.Designer.cs
+++ Arcanoid/Form1.Designer.cs
@@ -29,12 +29,52 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.MainWindow = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.MainWindow)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // MainWindow
+            // 
+            this.MainWindow.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.MainWindow.Location = new System.Drawing.Point(12, 12);
+            this.MainWindow.Name = "MainWindow";
+            this.MainWindow.Size = new System.Drawing.Size(200, 300);
+            this.MainWindow.TabIndex = 0;
+            this.MainWindow.TabStop = false;
+            this.MainWindow.Paint += new System.Windows.Forms.PaintEventHandler(this.MainWindow_Paint);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // timer2
+            // 
+            this.timer2.Enabled = true;
+            this.timer2.Interval = 25;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(332, 320);
+            this.Controls.Add(this.MainWindow);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            ((System.ComponentModel.ISupportInitialize)(this.MainWindow)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox MainWindow;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
     }
 }
 
--- Arcanoid/Form1.cs
+++ Arcanoid/Form1.cs
@@ -12,9 +12,64 @@ namespace Arcanoid
 {
     public partial class Form1 : Form
     {
+        private Point rectanglePos = new Point(85, 295);
+        private Point ellipseDirect = new Point(-10, -10);
+        private Rectangle rectangle = new Rectangle(90, 295, 20, 5);
+        private Rectangle ellipse = new Rectangle(97, 285, 5, 5);
+        private Point _direct = Point.Empty;
+
         public Form1()
         {
             InitializeComponent();
+            SetStyle(ControlStyles.OptimizedDoubleBuffer | ControlStyles.AllPaintingInWmPaint | ControlStyles.UserPaint, true);
+            UpdateStyles();
+        }
+
+        private void MainWindow_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics graph = e.Graphics;
+            Pen blackPen = new Pen(Color.FromArgb(255, 0, 0, 0), 5);
+
+            ellipse.X += ellipseDirect.X;
+            ellipse.Y += ellipseDirect.Y;
+
+            if (ellipse.X < 0 || ellipse.X > 200)
+            {
+                ellipseDirect.X *= -1;
+            }
+            if (ellipse.Y < 0 || ellipse.Y > 300)
+            {
+                ellipseDirect.Y *= -1;
+            }
+            graph.DrawEllipse(blackPen, ellipse);
+            graph.DrawRectangle(blackPen, rectangle);
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            Refresh();
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Left)
+            {
+                if (rectangle.X > 0)
+                {
+                    rectangle.X -= 15;
+                }
+            }
+            if (e.KeyCode == Keys.Right)
+            {
+                if (rectangle.X < 180)
+                {
+                    rectangle.X += 15;
+                }
+            }
         }
     }
 }
